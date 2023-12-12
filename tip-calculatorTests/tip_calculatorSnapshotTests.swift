b53a17e6f3c2a42d40bd6b44015f369da11fb85d
Mykhailo Radzievskyi <nn5n.com@gmail.com>
2023-12-12 11:40:10 +0200

    Lesson 30: - Added Image Snapshot Tests;

---
+++ tip-calculatorTests/tip_calculatorSnapshotTests.swift
@@ -0,0 +1,74 @@
+//
+//  tip_calculatorSnapshotTests.swift
+//  tip-calculatorTests
+//
+//  Created by Mykhailo Radzievskyi on 2023-12-12.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import tip_calculator
+
+final class tip_calculatorSnapshotTests: XCTestCase {
+    
+    private var screenWidth: CGFloat {
+        return UIScreen.main.bounds.size.width
+    }
+    
+    func testLogoView() {
+        // given
+        let size = CGSize(width: screenWidth, height: 48)
+        
+        // when
+        let view = LogoView()
+        
+        // then
+//        assertSnapshot(matching: view, as: .image(size: size), record: true) // Record snapshot for testing - first launch with "record: true", then remove it
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+    
+    func testInitialResultView() {
+        // given
+        let size = CGSize(width: screenWidth, height: 224)
+        
+        // when
+        let view = ResultView()
+        
+        // then
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+    
+    func testInitialBillInputView() {
+        // given
+        let size = CGSize(width: screenWidth, height: 56)
+        
+        // when
+        let view = BillInputView()
+        
+        // then
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+    
+    func testInitialTipInputView() {
+        // given
+        let size = CGSize(width: screenWidth, height: 56 + 56 + 16)
+        
+        // when
+        let view = TipInputView()
+        
+        // then
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+    
+    func testInitialSplitInputView() {
+        // given
+        let size = CGSize(width: screenWidth, height: 56)
+        
+        // when
+        let view = SplitInputView()
+        
+        // then
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+    
+}
